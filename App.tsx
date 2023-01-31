132b047a04091518670b5ec42ee5aa114e65b02a
guzeradimazz <Guzerashop@mail.ru>
2023-01-31 13:56:09 +0300

    initial

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  const titles:string[] = ['Loader','Tinder','Gesture Handler','RGB Slider']
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
